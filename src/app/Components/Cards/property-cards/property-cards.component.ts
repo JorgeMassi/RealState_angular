95199780bbb84627d2a664f3973f03509b8fee35
Jorge Massi <j.massi2022@gmail.com>
2024-07-28 16:12:14 +0100

    comit

--- src/app/Components/Cards/property-cards/property-cards.component.ts
+++ src/app/Components/Cards/property-cards/property-cards.component.ts
@@ -1,15 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Properties } from '../../../Model/Properties/properties';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-property-cards',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './property-cards.component.html',
-  styleUrl: './property-cards.component.css'
+  styleUrls: ['./property-cards.component.css']
 })
 export class PropertyCardsComponent {
-  @Input() property! :Properties
+  @Input() property!: Properties;
 
+  constructor(private router: Router) {} 
+
+  navigateToPropertyDetails() {
+    this.router.navigate(['/property-view', this.property.id]);
+  }
 }
